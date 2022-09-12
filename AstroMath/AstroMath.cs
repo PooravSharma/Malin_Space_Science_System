1c8243c6600157b4e17797533588fa0273f3f1ab
Poorav Sharma <57639207+PooravSharma@users.noreply.github.com>
2022-09-12 14:26:56 +0800

    Add files via upload

---
+++ AstroMath/AstroMath.cs
@@ -0,0 +1,32 @@
+﻿namespace AstroMath
+{
+    public class Calculate
+    {
+        static readonly int lightSpeed = 299792458;
+
+        public static double Star_Velocity(double restWavelength, double observedWavelength)
+        {
+
+            double changedWavelength = observedWavelength - restWavelength;
+            double velocity = (lightSpeed* changedWavelength)/restWavelength;
+            return velocity;
+        }
+        public static double Star_Distance(double arcseconds)
+        {
+            double distance = (1/arcseconds);
+            return distance;
+        }
+        public static double TemperatueChange(double celsius)
+        {   
+            double kelvin = celsius + 273;
+            return kelvin;
+
+        }
+        public static double EventHorizon(double blackholeMass)
+        {
+            double gravityConstant = 6.674*Math.Pow(10, -11);
+            double schwarzschildRadius = (2*blackholeMass*gravityConstant)/Math.Pow(lightSpeed,2);
+            return schwarzschildRadius;
+        }
+    }
+}
