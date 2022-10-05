a231f6f871d3ab98b92a366a345404ed7483dfb3
Poorav SHARMA <30045900@tafe.wa.edu.au>
2022-10-05 14:12:56 +0800

    Add project files.

---
+++ Malin_Space_Science_System/Malin_Space_Science_System.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Malin_Space_Science_System
+{
+    partial class Malin_Space_Science_System
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Malin_Space_Science_System
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Malin_Space_Science_System";
+            this.Text = "Form1";           
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Malin_Space_Science_System/Malin_Space_Science_System.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Malin_Space_Science_System
+{
+    public partial class Malin_Space_Science_System : Form
+    {
+        public Malin_Space_Science_System()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
